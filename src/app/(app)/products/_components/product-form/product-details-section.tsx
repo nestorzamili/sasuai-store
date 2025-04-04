a6023dadf7c6718f6f9c3b63709a476e729e47ce
nestorzamili <nestorzamili@gmail.com>
2025-04-04 22:16:31 +0700

    fix(product-details): set default value to 0 for numeric input on change

--- src/app/(app)/products/_components/product-form/product-details-section.tsx
+++ src/app/(app)/products/_components/product-form/product-details-section.tsx
@@ -98,7 +98,7 @@ export function ProductDetailsSection() {
                   {...field}
                   onChange={(e) => {
                     const value = e.target.valueAsNumber;
-                    field.onChange(Number.isNaN(value) ? '' : value);
+                    field.onChange(Number.isNaN(value) ? 0 : value);
                   }}
                 />
               </FormControl>
