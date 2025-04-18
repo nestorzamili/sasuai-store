9d76588bf442d8d384efd8a829cf60534084903d
ibobdb <bobynugraha19@gmail.com>
2025-04-18 23:14:01 +0700

    refactor(api): improve response structure for product retrieval
    This updates the GET response for product filtering to include a success flag and data object.

--- src/app/api/trasactions/route.ts
+++ src/app/api/trasactions/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server';
 import { ProductService } from '@/lib/services/product.service';
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get('search'); // e.g. `/api/search?search=hello`
+  const query = searchParams.get('search');
   try {
     const products = await ProductService.getProductFiltered({
       search: query || '',
@@ -15,10 +15,11 @@ export async function GET(request: NextRequest) {
 }
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  try {
-    const product = await ProductService.create(body);
-    return NextResponse.json(product);
-  } catch (error) {
-    console.log(error);
-  }
+  // return NextResponse.json(body);
+  // try {
+  //   const product = await ProductService.create(body);
+  //   return NextResponse.json(product);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 }
