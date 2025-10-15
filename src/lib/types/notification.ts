f0e250ded1fd9573842bdf5868ff59e55913d185
ibobdb <bobynugraha19@gmail.com>
2025-10-15 22:08:06 +0700

    feat(notification): add notification settings

--- src/lib/types/notification.ts
+++ src/lib/types/notification.ts
@@ -2,3 +2,8 @@ export type NotificationSettings = {
   is_active: boolean | false;
   api_key: string | '';
 };
+export type NotificationPayload = {
+  numbers: string[];
+  content: string;
+  api_key: string;
+};
